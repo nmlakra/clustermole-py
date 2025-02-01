39f64e3d5766ac8c402d4070873c4f47a33bb262
nmlakra <nikhilmarklakra@gmail.com>
2025-02-01 23:54:04 +0530

    refactored enrichr

--- cluster_mole_py/enrichr.py
+++ cluster_mole_py/enrichr.py
@@ -2,48 +2,93 @@ import requests
 import pandas as pd
 from typing import List, Dict
 import json
-from .utils import (
-    DEFAULT_ALL_CELL_TYPE_LIBRARIES,
-    DEFAULT_HS_CELL_TYPE_LIBRARIES,
-    DEFAULT_MM_CELL_TYPE_LIBRARIES,
-)
 
 
-ENRICHR_URL = "https://maayanlab.cloud/Enrichr/"
-SPEEDRICHR_URL = "https://maayanlab.cloud/speedrichr/"
+class Enrichr:
 
-def send_gene_list(gene_list: List[str], description: str) -> int:
+    ENRICHR_URL = "https://maayanlab.cloud/Enrichr/"
+    SPEEDRICHR_URL = "https://maayanlab.cloud/speedrichr/"
 
-    url = ENRICHR_URL + "addList"
-    payload = {
-            "list": (None, "\n".join(gene_list)),
-            "description": (None, description)
-    }
+    def __init__(
+        self,
+        gene_list: List[str],
+        pval_cutoff: float | None = None,
+        adj_pval_cutoff: float | None = None,
+        description: str = "",
+    ):
+        self.gene_list = gene_list
+        self.description = description
+        self.user_list_id = None
+        self.pval_cutoff = pval_cutoff
+        self.adj_pval_cutoff = adj_pval_cutoff
 
-    response = requests.post(url, files=payload)
+        if self.user_list_id is None:
+            self.send_gene_list()
 
-    if not response.ok:
-        raise Exception(f'Error sending gene list, status code: {response.status_code}')
+    def get_gene_list(self) -> List[str]:
+        return [gene.upper() for gene in self.gene_list if gene.isalnum()]
 
-    data = json.loads(response.text)
+    def send_gene_list(self) -> int:
 
-    if not data['userListId']:
-        raise ValueError('Could not submit gene list')
+        url = Enrichr.ENRICHR_URL + "addList"
+        if self.gene_list:
+            payload = {
+                "list": (None, "\n".join(self.get_gene_list())),
+                "description": (None, self.description),
+            }
+        else:
+            raise ValueError("Empty gene list")
 
-    return data['userListId']
+        response = requests.post(url, files=payload, verify=True)
 
-def get_enrichment_results(gene_list: List[str], gene_set_library: str, description: str = "") -> Dict[str, List]:
+        if not response.ok:
+            raise Exception(
+                f"Error sending gene list, status code: {response.status_code}"
+            )
 
-    gene_list = [gene.upper() for gene in gene_list]
-    url = ENRICHR_URL + "enrich"
-    params = {
-            'userListId': send_gene_list(gene_list, description),
-            'backgroundType': gene_set_library
-    }
+        data = json.loads(response.text)
 
-    response = requests.get(url, params)
+        if not data["userListId"]:
+            raise ValueError("Could not submit gene list")
 
-    if not response.ok:
-        raise Exception(f'Error fetching enrichment results, status code: {response.status_code}')
-    data = json.loads(response.text)
-    return data
+        self.user_list_id = data["userListId"]
+        return self.user_list_id
+
+    def get_enrichment_results(self, gene_set: str) -> pd.DataFrame:
+
+        url = Enrichr.ENRICHR_URL + "enrich"
+
+        params = {"userListId": self.user_list_id, "backgroundType": gene_set}
+
+        response = requests.get(url, params)
+
+        if not response.ok:
+            raise Exception(
+                f"Error fetching enrichment results, status code: {response.status_code}"
+            )
+        data = json.loads(response.text)
+
+        return self.format_enrichment_result(data, gene_set)
+
+    def format_enrichment_result(self,
+        enrichment_result: Dict, gene_set: str
+    ) -> pd.DataFrame:
+        cols = [
+            "rank",
+            "term name",
+            "p-value",
+            "odds ratio",
+            "combined score",
+            "overlapping genes",
+            "adjusted p-value",
+            "old p-value",
+            "old adjusted p-value",
+        ]
+        df = pd.DataFrame(enrichment_result[gene_set], columns=cols)
+        df["gene_set"] = gene_set
+
+        if self.adj_pval_cutoff:
+            return df[df["adjusted p-value"] <= self.adj_pval_cutoff]
+        elif self.pval_cutoff:
+            return df[df["p-value"] <= self.pval_cutoff]
+        return df
--- cluster_mole_py/utils.py
+++ cluster_mole_py/utils.py
@@ -1,17 +1,13 @@
 # Constants
 DEFAULT_ALL_CELL_TYPE_LIBRARIES = [
-    "CellMarker 2024",
-    "CellMarker Augmented 2021" "Descartes Cell Types and Tissue 2021",
-    "PanglaoDB Augmented 2021",
-    "Azimuth Cell Types 2021",
-    "Azimuth 2023",
-]
-
-DEFAULT_HS_CELL_TYPE_LIBRARIES = [
-    "Tabula Sapiens",
-    "Human Gene Atlas",
-]
-
-DEFAULT_MM_CELL_TYPE_LIBRARIES = [
-    "Tabula Muris" "Mouse Gene Atlas",
+    "CellMarker_2024",
+    "CellMarker_Augmented_2021",
+    "Descartes_Cell_Types_and_Tissue_2021",
+    "PanglaoDB_Augmented_2021",
+    "Azimuth_Cell_Types_2021",
+    "Azimuth_2023",
+    "Tabula_Sapiens",
+    "Human_Gene_Atlas",
+    "Tabula_Muris",
+    "Mouse_Gene_Atlas",
 ]
